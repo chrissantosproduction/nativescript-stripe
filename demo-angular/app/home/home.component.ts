a3db77a5e9e69cf8bb2332416908baa60ea6c0c8
chris <chrissantosproduction@gmail.com>
2017-10-25 13:30:13 -0400

    Updated demo

--- demo-angular/app/home/home.component.ts
+++ demo-angular/app/home/home.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { CreditCardView, Card, Stripe } from 'nativescript-stripe';
+import { Page } from 'ui/page';
 
 @Component({
     selector: 'HomeComponent',
     templateUrl: 'home/home.component.html'
 })
 export class HomeComponent {
+    constructor(
+        private _page: Page,
+    ) {
+    }
+
     public submit() {
-        console.log('Submitted');
+        let ccView: CreditCardView = this._page.getViewById('cardView');
+        let cc: Card = ccView.card;
+        //alert(JSON.stringify(cc));
+
+        let stripe: Stripe = new Stripe('pk_test_OHSX2noWHfjZMZ6uj0dbeSN7')
+        stripe.createToken(cc.card, function(err, token) {
+            alert(`${err} \n ${token} \n ${cc.number} ${cc.expMonth} ${cc.expYear} ${cc.cvc}`);
+        })
     }
 }
