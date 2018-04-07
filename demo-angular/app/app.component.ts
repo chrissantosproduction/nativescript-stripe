e06ba938acb40b88ae60bb9ed5e76995c48f1298
chris <chrissantosproduction@gmail.com>
2018-04-07 12:10:42 -0400

    Moved view register to main.ts in angular demo

--- demo-angular/app/app.component.ts
+++ demo-angular/app/app.component.ts
@@ -1,20 +1,9 @@
 import { Component } from '@angular/core';
-import {
-    registerElement,
-    isKnownView
-} from 'nativescript-angular/element-registry';
-import { CreditCardView } from 'nativescript-stripe-sdk';
 
 @Component({
     selector: 'ns-app',
     templateUrl: 'app.component.html'
 })
 export class AppComponent {
-    constructor() {
-        if (!isKnownView('CreditCardView')) {
-            registerElement('CreditCardView', () => {
-                return CreditCardView;
-            });
-        }
-    }
+    constructor() {}
 }
--- demo-angular/app/main.ts
+++ demo-angular/app/main.ts
@@ -2,9 +2,19 @@
 import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
 
 import { AppModule } from './app.module';
+import {
+    isKnownView,
+    registerElement
+} from 'nativescript-angular/element-registry';
+import { CreditCardView } from 'nativescript-stripe-sdk';
 
 // A traditional NativeScript application starts by initializing global objects, setting up global CSS rules, creating, and navigating to the main page.
 // Angular applications need to take care of their own initialization: modules, components, directives, routes, DI providers.
 // A NativeScript Angular app needs to make both paradigms work together, so we provide a wrapper platform object, platformNativeScriptDynamic,
 // that sets up a NativeScript application and can bootstrap the Angular framework.
+
+if (!isKnownView('CreditCardView')) {
+    registerElement('CreditCardView', () => CreditCardView);
+}
+
 platformNativeScriptDynamic().bootstrapModule(AppModule);
